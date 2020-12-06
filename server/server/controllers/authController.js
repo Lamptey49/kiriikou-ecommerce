53273dff1e9d50811216e2b605a24b2523893096
Lamptey Phinehas <lampteyphinehas70@gmail.com>
2020-12-06 09:43:39 +0800

    added bootstrap and other libraries

--- server/server/controllers/authController.js
+++ server/server/controllers/authController.js
@@ -87,6 +87,7 @@ const hasAuthorization = (req, res, next) => {
       })
     }
     next()
+    
     // const authHeader = req.headers.authorization;
     // if(authHeader){
     //   const token = authHeader.split(' ')[1]
@@ -94,11 +95,11 @@ const hasAuthorization = (req, res, next) => {
     //     if(err){
     //       return res.sendStatus(403)
     //     }
-    //     req.user = user
+        
     //   } );
     // }
     // else{
-    //   res.sendStatus(401)
+    //   req.user = user
     // }
   }
   
--- server/server/server.js
+++ server/server/server.js
@@ -28,35 +28,35 @@ io.on('connection', (socket)=>{
     })
 })
 
-mongoose.connect('mongodb://localhost:27017/kiriikou-db', {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://Phinehas:Phinehas86@cluster0.1gctm.mongodb.net/kiriikou-ecommerce?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true, useUnifiedTopology: true})
         .then(()=> console.log('Connected to the Database successfully'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
-app.use((req, res, next)=>{
-    if(req.headers['Authorization']){
-        const accessToken = req.headers['Authorization']
-        const { userId, exp } = jwt.verify( {
-           secret:config.jwtSecret,
-           getToken: function(req){
-                if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer'){
-                    return req.headers.authorization.split(' ')[1];
+// app.use((req, res, next)=>{
+//     if(req.headers['Authorization']){
+//         const accessToken = req.headers['Authorization']
+//         const { userId, exp } = jwt.verify( {
+//            secret:config.jwtSecret,
+//            getToken: function(req){
+//                 if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer'){
+//                     return req.headers.authorization.split(' ')[1];
 
-                }else if(req.query && req.query.token){
-                    return req.query.token
-                }
-                return null;
-            }})
-        if(exp < Date.now().valueOf() / 1000){
-            return res.status(401).json({ error: 'Your token has expired, please login to obtain new one'})
-        }
-        res.locals.loggedInUser =  User.findById(userId); 
-        next()
-    }
-    else{
-        next()
-    }
-})  
+//                 }else if(req.query && req.query.token){
+//                     return req.query.token
+//                 }
+//                 return null;
+//             }})
+//         if(exp < Date.now().valueOf() / 1000){
+//             return res.status(401).json({ error: 'Your token has expired, please login to obtain new one'})
+//         }
+//         res.locals.loggedInUser =  User.findById(userId); 
+//         next()
+//     }
+//     else{
+//         next()
+//     }
+// })  
 app.use('/', routes)
 
 app.use((err, req, res, next) => {
